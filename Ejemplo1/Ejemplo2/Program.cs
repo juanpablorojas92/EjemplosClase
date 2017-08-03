0da1c00454a7f5b763891b7aef45de0a16e5de90
juanpablorojas92 <juampa.22.06@gmail.com>
2017-08-03 13:16:14 +0200

    Sesion 7

---
+++ Ejemplo1/Ejemplo2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var a = 10;
+            var result = a.BetWeen(9, 20);
+            */
+
+            foreach (var i in Iterator())
+            {
+                Console.WriteLine(i);
+                
+            }
+            var numbers = new List<int>
+            {
+                1,2,3
+            };
+            var result = numbers.Where(Predicate);
+            /*
+            numbers.Where(n => n > 1);// Esto serviria para que no tengamos que crear el "Predicate" de abajo.
+            */
+            foreach(var value in result)
+            {
+                //Con esto conseguimos que entre en el "Predicate".
+            }
+
+            Console.WriteLine(result.GetType().Name);
+            Console.ReadLine();
+        }
+
+        static bool Predicate( int value)
+        {
+            Console.WriteLine("He entrado");
+            return value > 1;
+            
+        }
+
+        static IEnumerable<int> Iterator()
+        {
+            for(var i =0; i < 1; i++)
+            {
+                yield return i;
+            }
+            //"Yield" sirve para que devuelva el control a la linea del "foreach" sin necesidad de terminar el bucle. Es decir es como si anidaramos bucles.
+        }
+    }
+
+    public static class ExtensionInt
+    {
+        public static bool BetWeen(this int value, int from, int to) 
+        {
+            return value >= from && value <= to;
+        }
+    }
+
+}
+
