676a0f8daf1bc6961a62c1338af3b47b6c66b5e0
juanpablorojas92 <juampa.22.06@gmail.com>
2017-08-03 13:43:28 +0200

    Sesion 5.- Singleton

---
+++ Ejemplo1/Ejemplo3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //Patron Singleton 
+
+    public class Configuration
+    {
+        private static Configuration instance;
+        private Configuration()
+        {
+
+        }
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (null == instance) {
+                    instance = new Configuration();
+                }
+                return instance;
+            }
+        }
+    }
+}
