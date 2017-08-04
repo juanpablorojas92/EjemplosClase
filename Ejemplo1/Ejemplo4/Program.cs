56534c5fde993e05484a6caf177ecfd9c63fcc2f
juanpablorojas92 <juampa.22.06@gmail.com>
2017-08-04 12:10:42 +0200

    Factorias y nulos

---
+++ Ejemplo1/Ejemplo4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            //int b = null; //Esto no se permite porque no utilizar el nulable para int
+
+            System.Nullable<int> c = null; // Esto seria el código que se debería usar para hacer lo mismo que se hace poniendo "?" después del tipo de variable.
+        }
+    }
+
+    /*
+     * 
+     * Ejemplo de una clase que seria una factoria.
+     * 
+     * 
+    class Factory
+    {
+        IPolymorfica CreateConection(Type t)
+        {
+            return t.createInstance();
+        }
+    }
+    */
+}
