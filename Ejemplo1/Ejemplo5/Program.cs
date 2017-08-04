9150bb31259c5fb83b775fd19c4f02c44fadb7f9
juanpablorojas92 <juampa.22.06@gmail.com>
2017-08-04 13:29:10 +0200

    Ado.net

---
+++ Ejemplo1/Ejemplo5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Ejemplo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sql = @"Selec * 
+                       from tabla 
+                        where id = @id";//Con el arroba del principio podemos hacer saltos de linea en el código para que sea mas legible.
+            var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=pizzeria;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            try
+            {
+                connection.Open();
+    
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)//Siempre en el ultimo lugar. Las excepciones más especificas primeras. 
+            {
+                Console.WriteLine(e.GetType().Name);
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                connection.Dispose();
+                Console.WriteLine(connection.State);
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Ejemplo1/Ejemplo6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Ejemplo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=pizzeria;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) ;
+             {
+                 connection.Open();//Esto sirve para verificar que siempre hay que poner "try" y "catch"
+             };*/
+
+            using (var person = new Person())
+            {
+
+            }
+        }
+    }
+
+
+    class Person : IDisposable
+    {
+        public int Id { get; set; }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
